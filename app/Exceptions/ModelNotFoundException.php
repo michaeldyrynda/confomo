fe2aabd787ea789846f8b1969d779b261b4d1144
Michael Dyrynda <michael@dyrynda.com.au>
2025-07-27 12:11:15 +0930

    out with the old

--- app/Exceptions/ModelNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Exceptions;
-
-use Exception;
-
-class ModelNotFoundException extends Exception
-{
-}
--- resources/views/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div id="confomo-app" class="container" v-cloak>
-        <dashboard></dashboard>
-    </div>
-@endsection
